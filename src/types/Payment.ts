fd5817546147a3254f9ba5181c1db5479140cb5b
Matt <mcoplanwork@gmail.com>
2024-11-02 14:01:13 -0400

    Added a Payment type

---
+++ src/types/Payment.ts
@@ -0,0 +1,20 @@
+// Payment.ts
+import { Request } from './Request';  // Assuming the file name is Request.ts
+import { Medication } from './Medication';  // Assuming the file name is Medication.ts
+import { AuthenticationResponse } from './User';
+
+export enum PayStatus {
+    UNPAID = 'UNPAID',
+    FULLY_PAID = 'FULLY_PAID'
+}
+
+export interface Payment {
+    paymentId: number;
+    amount: number;
+    payStatus: PayStatus;
+    req: Request;
+    user: AuthenticationResponse;
+    med: Medication;
+    creationDate: Date;
+    updateDate: Date;
+}
