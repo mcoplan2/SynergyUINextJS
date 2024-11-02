edb59a888bbb5feb26145c07863fda06c91e961a
Matt <mcoplanwork@gmail.com>
2024-11-02 02:42:59 -0400

    Changed tailwind.config.ts to contain the correct theme colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,15 +2,16 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/**/*.{js,jsx,ts,tsx}', // Adjust this path if needed
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: '#00796B',
+        secondary: '#A5D6A7',
+        accent: '#4FC3F7',
+        neutral: '#E0E0E0',
+        highlight: '#01579B',
       },
     },
   },
