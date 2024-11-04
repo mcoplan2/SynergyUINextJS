56b14ec9cf137c59feb94f119c0ed47877bbeb05
Matt <mcoplanwork@gmail.com>
2024-11-03 20:08:32 -0500

    updatingg port

--- src/utils/API.ts
+++ src/utils/API.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from 'axios';
 
 // Define the base URL for your API
-const BASE_API_URL: string = "https://synergypharmacy-api-latest.onrender.com";
+const BASE_API_URL: string = "https://synergypharmacy-api-latest.onrender.com:8080";
 
 // Create a default Axios instance for public API calls
 let API: AxiosInstance = axios.create({
